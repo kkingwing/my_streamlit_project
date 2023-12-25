a241c4aa9bef585f63595a3de9c4e1634b76ab47
Qing Yi <495017921@qq.com>
2023-12-25 18:08:07 +0800

    dot

--- 查看版本.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-print("Streamlit version:", st.__version__)
-print("Pandas version:", pd.__version__)
-print("NumPy version:", np.__version__)
